05d4e86d9dc35fb8f03319eb416b6f9634a0fb6d
abdullahwahid2349-rgb <abdullahwahid2349@gmail.com>
2026-03-07 08:34:04 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,18 +18,18 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
-    };
-});build: {
-  chunkSizeWarningLimit: 1000,
-  rollupOptions: {
-    output: {
-      manualChunks(id) {
-        if (id.includes('node_modules')) {
-          return id.toString().split('node_modules/')[1].split('/')[0].toString();
+      },
+      build: {
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+            }
+          }
         }
       }
-    }
-  }
-}
-
+    };
+});
